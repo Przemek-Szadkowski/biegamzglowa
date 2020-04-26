a47687bb50b4d16a27de5964aab982ac2d0783af
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2020-04-26 22:04:56 +0200

    Build version

---
+++ build/precache-manifest.978a49c3c8139eeb4474335a62da89b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5a1fad41181459eb7101dabc277e23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce18a5d6735c0b5f9390",
+    "url": "/static/css/main.1128ec4d.chunk.css"
+  },
+  {
+    "revision": "daad0fe088bdd0fd97bc",
+    "url": "/static/js/2.a9664d8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9664d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce18a5d6735c0b5f9390",
+    "url": "/static/js/main.416f9430.chunk.js"
+  },
+  {
+    "revision": "0a0c07c9609ebb3fa5fd",
+    "url": "/static/js/runtime-main.9df535ad.js"
+  },
+  {
+    "revision": "6a91dbcdb346cb03e4b6b70f4dcdcc1e",
+    "url": "/static/media/landscape-78058_1024.6a91dbcd.jpg"
+  },
+  {
+    "revision": "fe070d5556e8ab6a8308830f8eb1ff4a",
+    "url": "/static/media/landscape-78058_1920.fe070d55.jpg"
+  },
+  {
+    "revision": "93f2a1506cb9e4f5ff2e41e89d2c7108",
+    "url": "/static/media/landscape-78058_800.93f2a150.jpg"
+  }
+]);
