fc6db8a45996f683ad21759d67ad0e73e02dedf7
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2021-07-11 14:54:51 +0200

    Updates

---
+++ precache-manifest.9eb4be7feed02db94b30d714bbca6aa3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ccad8f948d81b60ca23c84e446065e",
+    "url": "/biegamzglowa/index.html"
+  },
+  {
+    "revision": "458faedba8efd07ef4e5",
+    "url": "/biegamzglowa/static/css/main.407eb4ad.chunk.css"
+  },
+  {
+    "revision": "638f54233e5e34336de3",
+    "url": "/biegamzglowa/static/js/2.86ffd691.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/biegamzglowa/static/js/2.86ffd691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "458faedba8efd07ef4e5",
+    "url": "/biegamzglowa/static/js/main.181f7926.chunk.js"
+  },
+  {
+    "revision": "581e400d3fd8e9f3ab61",
+    "url": "/biegamzglowa/static/js/runtime-main.0b9f7248.js"
+  },
+  {
+    "revision": "6a91dbcdb346cb03e4b6b70f4dcdcc1e",
+    "url": "/biegamzglowa/static/media/landscape-78058_1024.6a91dbcd.jpg"
+  },
+  {
+    "revision": "fe070d5556e8ab6a8308830f8eb1ff4a",
+    "url": "/biegamzglowa/static/media/landscape-78058_1920.fe070d55.jpg"
+  }
+]);
